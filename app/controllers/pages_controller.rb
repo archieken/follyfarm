4f629388b0545cd759bd93c6353387511b9fba2e
archie kenwright <kenwright.aw@googlemail.com>
2018-04-03 01:34:58 +0200

    setup

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,13 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!, only: [:home, :about, :news]
 
   def home
   end
+
+  def about
+  end
+
+  def news
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/about', to: 'pages#about', as: 'about'
+  get '/news', to: 'pages#news', as: 'news'
+  resources :photos, only: [:index]
+
+
 end
