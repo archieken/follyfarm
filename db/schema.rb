a709ce2b9586a21bcbb2c3e08a5ad19387ca166c
archie kenwright <kenwright.aw@googlemail.com>
2018-04-19 00:48:17 +0200

    added calendar, and cycle routes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180416110147) do
+ActiveRecord::Schema.define(version: 20180417190119) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,14 @@ ActiveRecord::Schema.define(version: 20180416110147) do
   create_table "prices", force: :cascade do |t|
     t.string "subject"
     t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "seasons", force: :cascade do |t|
+    t.string "name"
+    t.datetime "start_time"
+    t.datetime "end_time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
