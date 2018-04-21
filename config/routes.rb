bd16aa72d1ec25bbd2b0e7d1ea9b998c65c534a6
archie kenwright <kenwright.aw@googlemail.com>
2018-04-21 18:44:13 +0200

    removed styling

--- config/routes.rb
+++ config/routes.rb
@@ -34,5 +34,6 @@ Rails.application.routes.draw do
   get '/local-walks-cycle-routes', to: redirect('/attractions#walks')
   get '/map', to: redirect('#find')
   get '/photo-gallery', to: redirect('/images')
+  get '/contact-us', to: redirect('#contact')
 
 end
