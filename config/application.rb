792b942ffce38ff728d0f110b812282f2ff8724d
archie kenwright <kenwright.aw@googlemail.com>
2019-05-18 15:22:39 +0200

    removed broken gems

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,6 @@ Bundler.require(*Rails.groups)
 module Follyfarm
   class Application < Rails::Application
 
-    config.assets.initialize_on_precompile = false
     config.generators do |generate|
       generate.assets false
       generate.helper false
